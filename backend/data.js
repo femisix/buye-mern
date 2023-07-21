bcd1a788fa146cf7083f9162a57182b6a3605879
Femi Ajayi <merceydeez2017@gmail.com>
2023-07-21 12:20:44 +0100

    backend-data

---
+++ backend/data.js
@@ -0,0 +1,50 @@
+const data = {
+  products: [
+    {
+      name: 'Nike shirt',
+      slug: 'nike-shirt',
+      image: '/attache/ashkan-forouzani-S5_t6VIMClE-unsplash.jpg', //679px x 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 19,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Nike pant',
+      slug: 'nike-pant',
+      image: '/attache/ivan-bogdanov-17qC7l19hMI-unsplash.jpg',
+      price: 114,
+      countInStock: 6,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 8,
+      description: 'high quality pants',
+    },
+    {
+      name: 'Adidas fit pant',
+      slug: 'adidas-fit-pant',
+      image: '/attache/engin-akyurt-oXYGKCYhQOI-unsplash.jpg',
+      price: 60,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality pant',
+    },
+    {
+      name: 'Adidas shirt',
+      slug: 'adidas-shirt',
+      image: '/attache/camilla-carvalho-l5gK18pujLE-unsplash (1).jpg',
+      price: 25,
+      countInStock: 15,
+      brand: 'Adidas',
+      rating: 4.5,
+      numReviews: 15,
+      description: 'high quality pant',
+    },
+  ],
+};
+
+export default data;
