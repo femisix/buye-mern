c8ea326021360684818560822742f02eeeb6df0d
Femi Ajayi <merceydeez2017@gmail.com>
2023-07-31 03:32:48 +0100

    order-status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import SignupScreen from './screens/SignupScreen';
 import ShipppingAddressScreen from './screens/ShipppingAddressScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -86,6 +87,7 @@ function App() {
               <Route path="/signin" element={<SigninScreen />} />
               <Route path="/signup" element={<SignupScreen />} />
               <Route path="/placeorder" element={<PlaceOrderScreen />} />
+              <Route path="/order/:id" element={<OrderScreen />} />
               <Route path="/shipping" element={<ShipppingAddressScreen />} />
               <Route path="/payment" element={<PaymentMethodScreen />} />
               <Route path="/product/:slug" element={<ProductScreen />} />
