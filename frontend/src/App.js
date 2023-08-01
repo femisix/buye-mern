6849dda664fefa358033dba95de12652e01fe11a
Femi Ajayi <merceydeez2017@gmail.com>
2023-08-01 14:55:22 +0100

    user-profile

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,6 +19,7 @@ import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
 import OrderHistoryScreen from './screens/OrderHistoryScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -29,6 +30,7 @@ function App() {
     localStorage.removeItem('userInfo'); //clear the localstorage from userinfo
     localStorage.removeItem('shippingAddress'); //clear the localstorage from shipping address
     localStorage.removeItem('paymentMethod');
+    window.location.href = '/signin'; //to redirect user to the sign in screen
   };
 
   return (
@@ -93,6 +95,7 @@ function App() {
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
               <Route path="/signup" element={<SignupScreen />} />
+              <Route path="/profile" element={<ProfileScreen />} />
               <Route path="/placeorder" element={<PlaceOrderScreen />} />
               <Route path="/order/:id" element={<OrderScreen />} />
               <Route path="/orderhistory" element={<OrderHistoryScreen />} />
